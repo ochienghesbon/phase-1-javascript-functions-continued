a98e1938ce4c730f1d86b0cbbc984d237e50caed
ochienghesbon <ochienghesbon@student.moringaschool.com>
2022-06-08 07:15:31 +0300

    commit tree 9c70d63bf7c2355d62ccfe025bfba6fc0a56eb6e

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(activity='roller-skate') {
+    return "This Saturday, I want to "+activity+ "!"
+}
+
+function mondayWork(activity = 'go to the office') {
+    return "This Monday, I will "+activity+"."
+}
+
+function wrapAdjective(style) {
+    let result =(function(activity) {
+        return "You are " +style+ activity+
+        style + "!"
+    })
+    let emphatic = result;
+    return emphatic;
+
+      
+}
